f63e1b7e4e9e69e50d44a23c08b8480f9676fe38
carocean <carocean.jofers@outlook.com>
2019-09-09 05:12:27 +0800

    1.0.3

--- cj.studio.network.nodeapp/src/cj/studio/network/nodeapp/NodeApplication.java
+++ cj.studio.network.nodeapp/src/cj/studio/network/nodeapp/NodeApplication.java
@@ -4,17 +4,25 @@ import cj.studio.ecm.EcmException;
 import cj.studio.ecm.IServiceProvider;
 import cj.studio.ecm.annotation.CjService;
 import cj.studio.network.*;
+import cj.studio.network.nodeapp.cluster.ClusterValve;
 import cj.studio.network.nodeapp.strategy.PasswordAuthenticateStrategy;
 import cj.studio.network.nodeapp.strategy.SystemAccessControllerStrategy;
 import cj.studio.util.reactor.IPipeline;
+import cj.studio.util.reactor.IValve;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 @CjService(name = "$.cj.studio.node.app", isExoteric = true)
 public class NodeApplication implements INodeApplication {
     IRBACConfig rbacconfig;
+    String masterNetworkName;
+    List<IValve> valves;
     @Override
     public void onstart(String home,String masterNetworkName, IServiceProvider site) {
+        this.masterNetworkName=masterNetworkName;
+        this.valves=new ArrayList<>();
         rbacconfig = new RBACConfig(masterNetworkName);
         try {
             rbacconfig.load(home);
@@ -46,10 +54,18 @@ public class NodeApplication implements INodeApplication {
 
     @Override
     public void oninactiveNetwork(INetwork network, IPipeline pipeline) {
-
+        for(IValve valve:valves){
+            pipeline.remove(valve);
+        }
     }
 
     @Override
     public void onactivedNetwork(UserPrincipal userPrincipal, INetwork network, IPipeline pipeline) {
+        if(masterNetworkName.equals(network.getInfo().getName())){
+            return;
+        }
+        IValve cluster=new ClusterValve();
+        this.valves.add(cluster);
+        pipeline.append(cluster);
     }
 }
